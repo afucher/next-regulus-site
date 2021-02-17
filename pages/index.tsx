fb52e7359d2c2ab8af056e03aef02bcd3394cd01
Arthur Fücher <arthurfucher@gmail.com>
2021-02-17 13:59:09 -0300

    corrige container

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Rodape from '../componentes/Rodape'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Regulus | Escola e Livraria especializada de Astrologia</title>
         <link rel="icon" href="/favicon.ico" />
